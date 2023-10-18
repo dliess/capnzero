8880e5245b8c610cc6c42aac5e5f5e72bdbcb0f8
dliess <liessdaniel415@gmail.com>
2023-10-18 13:05:45 +0000

    use 0.9.0 and better nvim support

--- .devcontainer/run_docker_image.sh
+++ .devcontainer/run_docker_image.sh
@@ -17,14 +17,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$DIR/common.sh"
 
 docker run \
-         --rm \
          --network=host \
          --mount type=bind,source=$HOME/.ssh,target=/home/build-user/.ssh \
          --mount type=bind,source=$HOME/.gitconfig,target=/home/build-user/.gitconfig \
          --mount type=bind,source=$HOME/.config/nvim,target=/home/$BUILD_USER/.config/nvim \
          --mount type=bind,source=$HOME/nvim-linux64,target=/home/$BUILD_USER/nvim-linux64 \
-         --mount type=bind,source=$REPO_DIR,target=/home/$BUILD_USER/s \
-       	 -ti $DOCKER_IMAGE_TAG:latest /bin/zsh
+         --mount type=bind,source=$REPO_DIR,target=/home/$BUILD_USER/capnzero \
+           -ti $DOCKER_IMAGE_TAG:latest /bin/zsh 
 
 ## logging in to a running container:
 ## get id with: docker ps
